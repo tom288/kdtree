280245c1027b0f8db0e8ed7c275de4d8c186b633
Tom Miller <tommmmm@protonmail.com>
2023-03-16 21:12:54 +0000

    Accelerate random tree generation

--- src/kdtree.c
+++ src/kdtree.c
@@ -10,50 +10,6 @@ const size_t rect_vertices = 6;
 const size_t vertex_floats = 5;
 const size_t rectangle_floats = rect_vertices * vertex_floats;
 
-// It is important that we pass a pointer to an array of vertices, because
-// the stb macros like arrput will write to the pointer for reallocation. By
-// passing a float* instead we would have dangling pointer issues outside.
-void dfs_bake_leaves(Node* node, float** vertices, size_t* leaves)
-{
-    Node* child = NULL;
-
-    for (size_t i = 0; i < 2; ++i)
-    {
-        if (node->children[i])
-        {
-            child = node->children[i];
-            dfs_bake_leaves(child, vertices, leaves);
-        }
-    }
-
-    if (child) return;
-
-    for (size_t vertex = 0; vertex < rect_vertices; ++vertex)
-    {
-        for (size_t floats = 0; floats < vertex_floats; ++floats)
-        {
-            float f;
-            if (floats < 2)
-            {
-                f = node->min_corner[floats];
-                if (
-                    (floats == 0 && vertex % 2 > 0) ||
-                    (floats == 1 && vertex > 0 && vertex < 4)
-                ) {
-                    f += node->size[floats];
-                }
-            }
-            else
-            {
-                f = node->colour[floats - 2];
-            }
-
-            arrput(*vertices, f);
-        }
-    }
-    (*leaves)++;
-}
-
 Graph kdtree_init(Shader* shader)
 {
     // Seed the random number generator
@@ -62,9 +18,9 @@ Graph kdtree_init(Shader* shader)
     // Allocate enough room to draw max_leaves nodes
     const size_t max_leaves = 1 << 16;
 
-    Node* nodes = NULL;
     // Guess the amount of memory needed to avoid both waste and growth
-    arrsetcap(nodes, max_leaves * 3);
+    Node* nodes = NULL;
+    arrsetcap(nodes, max_leaves * 1.65f);
 
     // Define the head of the k-d tree, which is never a leaf, so no colour
     arrput(nodes, ((Node) {
@@ -72,23 +28,19 @@ Graph kdtree_init(Shader* shader)
         .min_corner = { -1.0f, -1.0f },
         .size = { 2.0f, 2.0f },
         .split_axis = rand_bool(),
-        .split = rand_float(),
+        .split = 0.5f,
         .children = { NULL, NULL },
     }));
-    Node* head = &nodes[0];
 
     // Find a random child with value NULL
     // Assign a node to this child
     // Update the number of leaves
-    for (size_t leaves = 0; leaves < max_leaves;) // We increment conditionally
+    for (size_t leaves = arrlenu(nodes); leaves < max_leaves;)
     {
-        Node* node = head;
-        GLboolean child_index;
-
-        while (node->children[child_index = rand_bool()])
-        {
-            node = node->children[child_index];
-        }
+        size_t random_index = rand_int(arrlen(nodes), GL_TRUE);
+        Node* node = &nodes[random_index];
+        GLboolean child_index = rand_bool();
+        if (node->children[child_index]) child_index ^= 1;
 
         arrput(nodes, ((Node) {
             .colour = { 0.0f },
@@ -100,19 +52,9 @@ Graph kdtree_init(Shader* shader)
         }));
 
         Node* leaf = &arrlast(nodes);
-        node->children[child_index] = leaf;
         rand_vec3(leaf->colour);
-
-        if (node->children[!child_index])
-        {
-            ++leaves;
-        }
-        else
-        {
-            node->colour[0] = -1.0f;
-        }
-
         const GLboolean axis = node->split_axis;
+
         if (child_index)
         {
             leaf->min_corner[axis] += node->size[axis] * node->split;
@@ -122,12 +64,51 @@ Graph kdtree_init(Shader* shader)
         {
             leaf->size[axis] *= node->split;
         }
+
+        if (node->children[!child_index])
+        {
+            arrdelswap(nodes, random_index);
+            ++leaves;
+        }
+        else
+        {
+            node->children[child_index] = leaf;
+        }
     }
 
     float* vertices = NULL;
     arrsetcap(vertices, max_leaves * rectangle_floats);
-    size_t leaves = 0;
-    dfs_bake_leaves(nodes, &vertices, &leaves);
+
+    for (size_t i = 0; i < arrlenu(nodes); ++i)
+    {
+        const Node node = nodes[i];
+
+        if (node.children[GL_FALSE] || node.children[GL_TRUE]) continue;
+
+        for (size_t vertex = 0; vertex < rect_vertices; ++vertex)
+        {
+            for (size_t floats = 0; floats < vertex_floats; ++floats)
+            {
+                float f;
+                if (floats < 2)
+                {
+                    f = node.min_corner[floats];
+                    if (
+                        (floats == 0 && vertex % 2 > 0) ||
+                        (floats == 1 && vertex > 0 && vertex < 4)
+                    ) {
+                        f += node.size[floats];
+                    }
+                }
+                else
+                {
+                    f = node.colour[floats - 2];
+                }
+
+                arrput(vertices, f);
+            }
+        }
+    }
 
     // Free all remaining nodes
     arrfree(nodes);
