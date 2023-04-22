d7b7cee9e72ec3059164775f64863a93b43cb476
Tom Miller <tommmmm@protonmail.com>
2023-04-22 19:04:22 +0100

    Rename generate, add kdtree stuff for deterministic generation

--- src/kdtree.c
+++ src/kdtree.c
@@ -4,8 +4,34 @@
 #include <time.h> // time
 #include <stb_ds.h>
 
-Node node_child(Node parent, GLboolean child_index)
+// TODO import rules.h for nodetype, readrules etc (when it exists)
+
+Node randomn_node_child(Node parent, GLboolean child_index)
 {
+    Node child = {
+        .colour = { rand() & 255, rand() & 255, rand() & 255 },
+        .min_corner = { parent.min_corner[0], parent.min_corner[1] },
+        .size = { parent.size[0], parent.size[1] },
+        .split_axis = rand_bool(),
+        .split = bias_float(rand_float(), 0.5, 2.0f),
+    };
+
+    const GLboolean axis = parent.split_axis;
+    float split = parent.split;
+
+    if (child_index)
+    {
+        child.min_corner[axis] += parent.size[axis] * split;
+        split = 1.0f - split;
+    }
+    child.size[axis] *= split;
+
+    return child;
+}
+
+Node randomn_node_child(Node parent, GLboolean child_index, NodeType* rules)
+{
+    // TODO use rules
     Node child = {
         .colour = { rand() & 255, rand() & 255, rand() & 255 },
         .min_corner = { parent.min_corner[0], parent.min_corner[1] },
@@ -51,6 +77,42 @@ Node* gen_random_nodes(float min_area)
         const Node node = nodes[num_nodes_finished];
         if (node.size[0] * node.size[1] > min_area)
         {
+            arrput(nodes, random_node_child(node, 0));
+            nodes[num_nodes_finished] = random_node_child(node, 1);
+        }
+        else num_nodes_finished++;
+    }
+
+    return nodes;
+}
+
+Node* gen_nodes(float min_area)
+{
+    NodeType* types = readRules(); // reading in node types text file
+
+    // Guess the amount of memory needed to avoid both waste and growth
+    Node* nodes = NULL;
+    size_t expected_node_count = 6.0f / min_area;
+    arrsetcap(nodes, expected_node_count);
+
+    // TODO define world node from rules
+    // Define the head of the k-d tree, which is never a leaf, so has no colour
+    arrput(nodes, ((Node) {
+        .colour = { 0 },
+        .min_corner = { -1.0f, -1.0f },
+        .size = { 2.0f, 2.0f },
+        .split_axis = rand_bool(),
+        .split = bias_float(rand_float(), 0.5, 2.0f),
+    }));
+
+    size_t num_nodes_finished = 0;
+
+    // Expand all nodes until they are finished
+    while (arrlenu(nodes) > num_nodes_finished)
+    {
+        const Node node = nodes[num_nodes_finished];
+        if (node.size[0] * node.size[1] > min_area)
+        {
             arrput(nodes, node_child(node, 0));
             nodes[num_nodes_finished] = node_child(node, 1);
         }
@@ -60,7 +122,7 @@ Node* gen_random_nodes(float min_area)
     return nodes;
 }
 
-void** gen_vertices(Node* nodes)
+void** gen_vertices_from_nodes(Node* nodes)
 {
     float* vertex_floats = NULL;
     arrsetcap(vertex_floats, arrlenu(nodes) * 4);
@@ -91,7 +153,15 @@ void** gen_random_vertices()
     const size_t target_node_count = 1000 * 1000;
     const float min_area = 5.992f / target_node_count;
     Node* nodes = gen_random_nodes(min_area);
-    return gen_vertices(nodes);
+    return gen_vertices_from_nodes(nodes);
+}
+
+void** gen_vertices()
+{
+    const size_t target_node_count = 1000 * 1000;
+    const float min_area = 5.992f / target_node_count;
+    Node* nodes = gen_random_nodes(min_area);
+    return gen_vertices_from_nodes(nodes);
 }
 
 Graph kdtree_init(Shader* shader)
