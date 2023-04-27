cdde85869022611dd6ad58934becb989202fcd72
Tom Miller <tommmmm@protonmail.com>
2023-04-27 19:10:55 +0100

    Move random functions

--- src/rules.c
+++ src/rules.c
@@ -1,6 +1,6 @@
 extern int WIP;
 #include "kdtree.h"
-//#include "queue.h"
+#include "utility.h"
 #include "str_util.h"
 #include <cglm/cglm.h>
 #include <stb_ds.h>
@@ -122,26 +122,4 @@ NodeType* readRules()
     };
 
     return types;
-}
-
-size_t rotate_left(size_t n, size_t dist) {
-    return (n << dist) | (n >> (sizeof(size_t) * 8 - dist));
-}
-
-size_t rotate_left_quarter(size_t n, size_t count) {
-    return rotate_left(n, count * sizeof(size_t) * 2);
-}
-
-typedef union {
-    float from;
-    size_t to;
-} float_to_size;
-
-// xor((int32)X, (int32)Y >> 8, (int32)width >> 16, (int32)height >> 24)
-size_t sampleRandom(Node sample) { // uint64_t
-    float_to_size x = {.from = sample.min_corner[0]};
-    float_to_size y = {.from = sample.min_corner[1]};
-    float_to_size width = {.from = sample.size[0]};
-    float_to_size height = {.from = sample.size[1]};
-    return x.to ^ rotate_left_quarter(y.to, 1) ^ rotate_left_quarter(width.to, 2) ^ rotate_left_quarter(height.to, 3);
 }
