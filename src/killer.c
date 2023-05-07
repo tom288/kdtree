b364a1d2dbf75a08edcbd60bd2d907a3ac63acbd
Tom Miller <tommmmm@protonmail.com>
2023-05-07 17:30:02 +0100

    Fix killer and document main

--- src/killer.c
+++ src/killer.c
@@ -8,25 +8,25 @@ Killer killer_init()
     };
 }
 
-int killer_assert(Killer killer, GLboolean b)
+int killer_assert(Killer* killer, GLboolean b)
 {
     if (!b) killer_kill(killer);
     return b;
 }
 
-void killer_target(Killer killer, kill_fn fn, void* arg)
+void killer_target(Killer* killer, kill_fn fn, void* arg)
 {
-    arrput(killer.kill_fns, (kill_fn)fn);
-    arrput(killer.kill_args, arg);
+    arrput(killer->kill_fns, (kill_fn)fn);
+    arrput(killer->kill_args, arg);
 }
 
-int killer_kill(Killer killer)
+int killer_kill(Killer* killer)
 {
-    for (size_t i = 0; i < arrlenu(killer.kill_fns); ++i)
+    for (size_t i = 0; i < arrlenu(killer->kill_fns); ++i)
     {
-        (killer.kill_fns[i])(killer.kill_args[i]);
+        killer->kill_fns[i](killer->kill_args[i]);
     }
-    arrfree(killer.kill_fns);
-    arrfree(killer.kill_args);
+    arrfree(killer->kill_fns);
+    arrfree(killer->kill_args);
     return 0;
 }
